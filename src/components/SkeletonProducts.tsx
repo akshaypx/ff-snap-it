505d5cc05e7fccbb01cfa129d0dd6f6ad46f3bcd
akshaypx <akshaysrivastava46@gmail.com>
2025-05-29 21:17:43 +0530

    fix: better ui

--- src/components/SkeletonProducts.tsx
+++ src/components/SkeletonProducts.tsx
@@ -2,13 +2,21 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const SkeletonProducts = () => (
   <div className="max-w-7xl w-full flex-1 mx-auto px-4 py-12 space-y-6">
-    <Skeleton className="h-10 w-1/3" />
+    <div className="flex items-center space-x-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {Array.from({ length: 6 }).map((_, idx) => (
-        <div key={idx} className="border rounded-xl p-4 space-y-4 bg-white">
-          <Skeleton className="h-40 w-full rounded-md" />
-          <Skeleton className="h-4 w-3/4" />
-          <Skeleton className="h-4 w-1/2" />
+        <div key={idx} className="flex flex-col space-y-3">
+          <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
+          </div>
         </div>
       ))}
     </div>
